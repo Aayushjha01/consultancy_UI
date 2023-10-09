f2bf562ae20dfebd851c5e502ccbf4f783a8a5a1
Aayushjha01 <ajha15704@gmail.com>
2023-10-09 22:34:30 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [formData, setFormData] = useState({
+    
+    name: '',
+    address:'',
+    email: '',
+    password:'',
+    phone:''
+   
+  });
+
+  const handleChange = (e) => {
+    
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('http://192.168.18.15:4500/users/createUser', formData);
+
+     
+      console.log('Response:', response.data);
+    } catch (error) {
+      
+      console.error('Error:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>POST Request Example</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="address">Address:</label>
+          <input
+            type="text"
+            id="address"
+            name="address"
+            value={formData.address}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="text"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div>
+      <label htmlFor="password">password:</label>
+      <input
+        type="text"
+        id="password"
+        name="password"
+        value={formData.password}
+        onChange={handleChange}
+      />
+    </div>
+
+    <div>
+    <label htmlFor="phone">Phone:</label>
+    <input
+      type="text"
+      id="phone"
+      name="phone"
+      value={formData.phone}
+      onChange={handleChange}
+    />
+  </div>
+
+      
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
     </div>
   );
 }
